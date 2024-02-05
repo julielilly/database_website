cfc300f69192af14c513944913bbef85a276d122
Julie <julieglion@gmail.com>
2024-02-05 09:19:51 +0100

    first commit

---
+++ produktlist.js
@@ -0,0 +1,36 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  //looper og kalder showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  console.log(product);
+  //fang template
+  const template = document.querySelector("#small_product_template").content;
+
+  //lav en kopi
+  const copy = template.cloneNode(true);
+
+  //ændre indhold
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector("p").textContent = product.productdisplayname;
+  copy.querySelector(".amount").textContent = product.price;
+  copy.querySelector(".discount").textContent = product.discount + "%";
+
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldout");
+    copy.querySelector(".notinstock").classList.remove("hide");
+  }
+
+  if (product.discount) {
+    copy.querySelector("article").classList.add("sale");
+    copy.querySelector(".discount").classList.remove("hide");
+  }
+
+  //append
+  document.querySelector("main").appendChild(copy);
+}
